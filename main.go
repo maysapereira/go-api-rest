79dbb143fcdc7af8bc3135c41f7368681b9cec37
maysapereira <maysalvespereira@gmail.com>
2022-07-14 10:31:41 -0300

    :sparkles: cria função de home page e uma respectiva rota

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
 func main() {
 	fmt.Println("Iniciando projeto: API REST em GO")
+	HandleRequest()
 }
