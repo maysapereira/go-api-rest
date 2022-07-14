40843ff3ec1e77c7bd1ce161846dff306274eb4e
maysapereira <maysalvespereira@gmail.com>
2022-07-14 16:42:10 -0300

    :card_file_box: adiciona banco de dados postgres com docker

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,26 @@
+create table personagens(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personagens(nome, historia) VALUES
+('So Moon', 'So Moon é um estudante do ensino médio e o mais novo caçador, 
+e trabalha meio período no Eonni Noodles. Fisicamente deficiente após um 
+acidente de carro que matou seus pais há sete anos, ele se junta aos Caçadores
+depois de ser possuído pelo espírito Yung Wi-gen devido ao seu forte senso de 
+justiça, responsabilidade e desejo de proteger aqueles que lhe são queridos. 
+Ele era frequentemente capaz de convocar o Yung e foi escolhido em um estado 
+saudável devido à sua raiva e frustração acumuladas que poderiam destruir todos 
+os espíritos malignos. Como Caçador, ele possui velocidade sobre-humana, psicometria 
+de curto alcance e a capacidade de sentir espíritos malignos que entram no Território
+de Yung. Mais tarde, ele adquirepsicocinese e a capacidade de tocar e convocar o 
+Território à vontade. Moon tem um forte talento em desenho e atualmente está 
+ilustrando um webtoon de super -heróis ao lado de seus melhores amigos Woong-min 
+e Joo-yeon.'),
+('Ha-na', 'Hana-na é uma Caçadora e trabalha no Eonni Noodles. A única sobrevivente 
+depois que ela e sua família foram envenenadas, Ha-na se juntou aos Caçadores depois 
+de ser possuída por Woo-Sik, que lhe concedeu força e psicometria sobre-humanas. Ela 
+é capaz de sentir espíritos malignos mesmo a centenas de quilômetros de distância, 
+além de ser capaz de ler e viver memórias de anos atrás como se estivesse 
+fisicamente lá.');
